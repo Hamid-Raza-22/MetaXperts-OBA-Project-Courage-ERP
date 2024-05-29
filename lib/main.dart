e3f86571f41e93fc248dc636aafbce9e29f95cfb
Hamid Raza <hamidraza.engr@gmail.com>
2024-05-29 12:21:17 +0500

    29-MAY-2024- 12:18 pm

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,7 @@ Future<void> main() async {
 
   WidgetsFlutterBinding.ensureInitialized();
    Upgrader;
-
+// hamid
   // // AndroidAlarmManager.initialize();
   //
   // Initialize the FlutterBackground plugin
